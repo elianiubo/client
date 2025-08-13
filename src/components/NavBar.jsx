35b3c8b406131a7a5721cee9e98fe4e6a7217cf0
Elia <elia.niubo@gmail.com>
2025-08-13 19:03:25 +0200

    NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import "../styles/NavBar.css";
+import "../styles/NavBar.css"; // Asegúrate de tener este archivo CSS
 import {  ENDPOINTS } from "../config/index";
 
 export default function NavBar({ update }) {
