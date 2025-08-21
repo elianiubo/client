59a27520a9a8a63d10a2bc55b6a73b175d5846e1
Elia <elia.niubo@gmail.com>
2025-08-21 14:09:21 +0200

    latest imrpovements

--- src/components/ImageUploadForm.jsx
+++ src/components/ImageUploadForm.jsx
@@ -55,6 +55,7 @@ export default function ImageUploadForm({ triggerUpdate }) {
           Authorization: `Bearer ${token}`,
           "Content-Type": "multipart/form-data",
         },
+         withCredentials: true
       });
 
       alert("✅ Imágenes subidas con éxito");
